080ff0e8c4254c6d13407d01c3f03732854c54e4
tanishk kushwaha <149100136+droid85real@users.noreply.github.com>
2025-08-26 14:00:02 +0530

    hashed password using bcrypt

--- server/src/config/env.js
+++ server/src/config/env.js
@@ -6,3 +6,4 @@ dotenv.config();
 
 export const PORT=process.env.PORT || 5000;
 export const CLIENT_URL=process.env.CLIENT_URL || "http://localhost:5173";
+export const PEPPER_SECRET=process.env.PEPPER_SECRET || "some_random_string";
--- server/src/modules/users/user.controller.js
+++ server/src/modules/users/user.controller.js
@@ -3,30 +3,38 @@ import UserRepository from "./user.repository.js";
 
 export default class UserController {
   // sign Up middleware
-  postSignUp(req, res) {
-    const result = UserRepository.signUp(req.body);
-
-    if (result.status === "EMAIL_ALREADY_REGISTERED") {
-      return res.status(409).json({ error: "Email already registered" });
-    } else if (result.status === "SUCCESS") {
-      return res.status(201).json(result.newUser);
-    } else {
-      return res.status(500).json({ error: "Something went wrong" });
+  async postSignUp(req, res) {
+    try {
+      const result = await UserRepository.signUp(req.body);
+  
+      if (result.status === "EMAIL_ALREADY_REGISTERED") {
+        return res.status(409).json({ error: "Email already registered" });
+      } else if (result.status === "SUCCESS") {
+        return res.status(201).json(result.newUser);
+      } else {
+        return res.status(500).json({ error: "Something went wrong" });
+      }
+    } catch (error) {
+      return res.status(500).json({error: error.message });
     }
   }
 
   // sign In middleware
-  postSignIn(req, res) {
-    const result = UserRepository.signIn(req.body);
-
-    if (result.status === "USER_NOT_FOUND") {
-      return res.status(404).json({ error: "User not found" });
-    } else if (result.status === "INCORRECT_PASSWORD") {
-      return res.status(401).json({ error: "Incorrect password" });
-    } else if (result.status === "SUCCESS") {
-      return res.status(200).json(result.user);
-    } else {
-      return res.status(500).json({ error: "Something went wrong" });
+  async postSignIn(req, res) {
+    try {
+      const result = await UserRepository.signIn(req.body);
+  
+      if (result.status === "USER_NOT_FOUND") {
+        return res.status(404).json({ error: "User not found" });
+      } else if (result.status === "INCORRECT_PASSWORD") {
+        return res.status(401).json({ error: "Incorrect password" });
+      } else if (result.status === "SUCCESS") {
+        return res.status(200).json(result.user);
+      } else {
+        return res.status(500).json({ error: "Something went wrong" });
+      }
+    } catch (error) {
+      return res.status(500).json({ error: error.message });
     }
   }
 }
