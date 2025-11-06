ab4dea8f7332605898ad2f7925f4eacd40eba7d0
tanishk kushwaha <149100136+droid85real@users.noreply.github.com>
2025-11-06 22:48:26 +0000

    cors backend console

--- server/src/config/cors.js
+++ server/src/config/cors.js
@@ -5,5 +5,7 @@ export const corsOptions={
     origin: CLIENT_URL,
     // credentials: true // if you’ll ever use cookies/auth
 };
+// In your backend, check what CLIENT_URL is set to
+console.log('CLIENT_URL:', process.env.CLIENT_URL);
 
 export default cors(corsOptions);
