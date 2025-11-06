0afbd35c132340c4a9734605806326c7ba898550
tanishk kushwaha <149100136+droid85real@users.noreply.github.com>
2025-11-06 22:40:58 +0000

    api consolelog

--- client/src/api.js
+++ client/src/api.js
@@ -4,3 +4,8 @@
 export const API_BASE = import.meta.env.PROD
   ? import.meta.env.VITE_API_URL // production
   : ""; // development (proxy)
+
+  // Temporary debug
+console.log('PROD:', import.meta.env.PROD);
+console.log('VITE_API_URL:', import.meta.env.VITE_API_URL);
+console.log('Full import.meta.env:', import.meta.env);
